cd14c84eb6aecf57d2bc4c73f9a17ec2d3cf82dd
danirguez <32953186+danirguez@users.noreply.github.com>
2018-04-04 17:32:09 +0200

    feat: repositorios

---
+++ Acme-Newspaper/src/main/java/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+	@Query("select u from User u join u.userAccount uc where uc.id = ?1")
+	User findByPrincipal(int id);
+
+}
